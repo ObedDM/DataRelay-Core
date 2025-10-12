af8771b7a43835b4eaa45fc0a1b6ea9b4fb5b0a9
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-10-11 20:52:53 -0600

    Spring initializr setup

---
+++ src/main/java/com/datarelay/core/DataRelayCoreApplication.java
@@ -0,0 +1,13 @@
+package com.datarelay.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRelayCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRelayCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datarelay/core/DataRelayCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datarelay.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataRelayCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
