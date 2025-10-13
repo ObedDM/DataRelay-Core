8dd5cd13e0476e03c39da30b6e4d55048a88b0cb
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-10-13 01:58:58 -0600

    Add: create-user endpoint

--- src/main/java/com/datarelay/core/controller/DataController.java
+++ src/main/java/com/datarelay/core/controller/DataController.java
@@ -1,14 +1,38 @@
 package com.datarelay.core.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.datarelay.core.entity.Users;
+import com.datarelay.core.services.UserService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
 public class DataController {
+
+    @Autowired
+    private UserService UserService;
 
     @GetMapping("/")
     public String home() {
         return "DataRelay API endpoint test";
     }
+
+    @PostMapping("/create-user")
+    public ResponseEntity<Users> createUser(@RequestBody Users userRequest) {
+        UserService.createNewUser(
+            userRequest.getUsername(),
+            userRequest.getPassword()
+        );
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    
     
 }
