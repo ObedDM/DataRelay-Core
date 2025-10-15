a24a6bc032b2bac315ee46c24461443bf91b2873
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-10-15 15:49:58 -0600

    Add: Getters and Setters for DatasetSchema entity

--- src/main/java/com/datarelay/core/entity/DatasetSchema.java
+++ src/main/java/com/datarelay/core/entity/DatasetSchema.java
@@ -43,4 +43,44 @@ public class DatasetSchema {
 
     }
 
+    public UUID getSchemaId() {
+        return schemaId;
+    }
+
+    public void setSchemaId(UUID schemaId) {
+        this.schemaId = schemaId;
+    }
+
+    public Users getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Users userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;        
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
