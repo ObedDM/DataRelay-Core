d176f2cd9cdbb011cde8690838cec7d16197ebab
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-10-15 18:13:08 -0600

    Remove: unused imports

--- src/main/java/com/datarelay/core/entity/Columns.java
+++ src/main/java/com/datarelay/core/entity/Columns.java
@@ -12,7 +12,6 @@ import jakarta.persistence.ForeignKey;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 
