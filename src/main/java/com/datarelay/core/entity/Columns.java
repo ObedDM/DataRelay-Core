449f070b6fa5e4514ab04358bc768a6c51b9e856
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-10-15 18:08:18 -0600

    Fix: Fixed Columns entity to work as a weak sql table

--- src/main/java/com/datarelay/core/entity/Columns.java
+++ src/main/java/com/datarelay/core/entity/Columns.java
@@ -1,10 +1,17 @@
 package com.datarelay.core.entity;
 
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -12,20 +19,24 @@ import jakarta.persistence.UniqueConstraint;
 @Entity
 @Table(
     name = "columns",
-    uniqueConstraints = @UniqueConstraint(columnNames = {"schema_id, name"}))
+    uniqueConstraints = @UniqueConstraint(columnNames = {"schema_id", "name"}))
 public class Columns {
+
+    @EmbeddedId
+    private ColumnsId id;
  
     public Columns() {
 
     }
 
+    @MapsId("schemaId")
     @ManyToOne
     @JoinColumn(
         name = "schema_id",
         foreignKey = @ForeignKey(name = "FK_COLUMNS_SCHEMA"),
         nullable = false
         )
-    private DatasetSchema schemaId;
+    private DatasetSchema schema;
 
     @OneToOne
     @JoinColumn(
@@ -34,34 +45,31 @@ public class Columns {
         )
     private Dtype dtype;
 
-    @Column(name = "name", columnDefinition = "TEXT", nullable = false)
-    private String name;
-
     @Column(name = "position", columnDefinition = "SMALLINT", nullable = false)
     private int position;
 
-    public DatasetSchema getSchemaId() {
-        return schemaId;
+    public ColumnsId getId() {
+        return id;
     }
 
-    public void setSchemaId(DatasetSchema schemaId) {
-        this.schemaId = schemaId;
+    public void setId(ColumnsId id) {
+        this.id = id;
+    }
+
+    public DatasetSchema getSchema() {
+        return schema;
+    }
+
+    public void setSchema(DatasetSchema schema) {
+        this.schema = schema;
     }
 
     public Dtype getDtype() {
         return dtype;
     }
 
-    public void SetDtype(Dtype dtype) {
+    public void setDtype(Dtype dtype) {
         this.dtype = dtype;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public int getPosition() {
@@ -70,5 +78,45 @@ public class Columns {
 
     public void setPosition(int position) {
         this.position = position;
+    }
+
+    @Embeddable
+    public static class ColumnsId implements Serializable {
+
+        @Column(name = "schema_id", nullable = false)
+        private UUID schemaId;
+        
+        @Column(name = "name", columnDefinition = "TEXT", nullable = false)
+        private String name;
+        
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public UUID getSchemaId() {
+            return schemaId;
+        }
+
+        public void setSchemaId(UUID schemaId) {
+            this.schemaId = schemaId;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof ColumnsId that)) return false;
+            return Objects.equals(schemaId, that.schemaId) &&
+                Objects.equals(name, that.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(schemaId, name);
+        }
+
     }
 }
