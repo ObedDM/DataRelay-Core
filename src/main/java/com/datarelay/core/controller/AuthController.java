aa1f3dc9e148025cec5e6332037b745dbbbc1cae
ObedDM <pacocobot2@gmail.com>
2026-09-18 20:47:20 -0600

    feat: add swagger docs to auth controller

--- src/main/java/com/datarelay/core/controller/AuthController.java
+++ src/main/java/com/datarelay/core/controller/AuthController.java
@@ -10,11 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.datarelay.core.dto.UserDTO;
 import com.datarelay.core.service.rest.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+@Tag(name = "Authentication", description = "User authentication REST controller")
 @Slf4j
 @RestController
 @RequestMapping("/auth")
@@ -22,6 +29,11 @@ import reactor.core.publisher.Mono;
 public class AuthController {
     private final UserService userService;
 
+    @Operation(summary = "User login", description = "Handles user credentials and logs in user, returning a token")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "User logged in"),
+        @ApiResponse(responseCode = "401", description = "Invalid user credentials", content = @Content(mediaType = "text/plain", schema = @Schema(type = "string", example = "Invalid credentials")))
+    })
     @PostMapping("/login")
     public Mono<ResponseEntity<String>> login(@Valid @RequestBody UserDTO credentials) {
         String username = credentials.username();
