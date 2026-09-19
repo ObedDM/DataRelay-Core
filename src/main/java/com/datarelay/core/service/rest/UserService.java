146a4c1919af41a18734012456b8918b7f6fb652
ObedDM <pacocobot2@gmail.com>
2026-09-19 02:31:46 -0600

    add javadocs for UserService interface

--- src/main/java/com/datarelay/core/service/rest/UserService.java
+++ src/main/java/com/datarelay/core/service/rest/UserService.java
@@ -5,7 +5,22 @@ import com.datarelay.core.entity.User;
 import reactor.core.publisher.Mono;
 
 public interface UserService {
+
+    /**
+     * Saves a new user in the database
+     * 
+     * @param username unique new user handle
+     * @param password plaintext password to hash and store
+     * @return a Mono with the newly created {@link User}
+     */
     public Mono<User> createNewUser(String username, String password);
 
+    /**
+     * Logs in user
+
+     * @param username unique user handle
+     * @param password plaintext user password
+     * @return a Mono containing JWT as a String
+     */
     public Mono<String> login(String username, String password);
 }
